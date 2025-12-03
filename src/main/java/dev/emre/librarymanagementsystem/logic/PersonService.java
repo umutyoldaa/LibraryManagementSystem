f8b92d02bacb0d075e2b0260450eb2a285b80289
umutyoldaa <88593075+umutyoldaa@users.noreply.github.com>
2025-12-03 18:30:04 +0100

    feat: add PersonService with basic validation

---
+++ src/main/java/dev/emre/librarymanagementsystem/logic/PersonService.java
@@ -0,0 +1,33 @@
+package dev.emre.librarymanagementsystem.logic;
+
+import dev.emre.librarymanagementsystem.models.Person;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PersonService {
+    private final List<Person> persons = new ArrayList<>();
+    private long nextId = 1;
+    public List<Person> getAllPersons(){
+        return new ArrayList<>(persons);
+    }
+    public Optional<Person> getPersonById(long id){
+        return persons.stream().filter(person -> person.getId() == id).findFirst();
+    }
+    public void addPerson(Person person){
+        if(person == null)return;
+        if(person.getName() == null || person.getSurname() == null || person.getBirthDate() == null || person.getAdress() == null){
+            return;
+        }
+        person.setId(nextId++);
+        persons.add(person);
+    }
+    public boolean deletePerson(long id){
+        return persons.removeIf(person -> person.getId() == id);
+    }
+    public Optional<Person> getPersonByName(String name){
+        return persons.stream().filter(person -> person.getName().equals(name)).findFirst();
+    }
+}
